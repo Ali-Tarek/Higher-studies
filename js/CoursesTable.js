f573b7f0a0fb2bbaecb4dfac3abb0f1042daad15
markayman15 <markayman453@gmail.com>
2023-05-31 16:48:26 +0300

    add search bar and ajax part of course table

--- js/CoursesTable.js
+++ js/CoursesTable.js
@@ -2,16 +2,45 @@ function WriteTable() {
     var render = "<table class = \"courses\">";
     render += "<thead><tr><th>Course Name</th><th>Course ID</th><th>Number of hours</th><th>Department</th><th>Lecture Day</th><th>Hall Number</th></thead>";
     render+="<tbody>";
-    var data = JSON.parse(localStorage.getItem('courses'));
-    for (i = 0; i < data.length; i++) {
-        render += "<tr><td>" + data[i].name + "</td><td>" + data[i].id + " </td>";
-        render += "<td>" + data[i].numberOfHours + "</td>";
-        render += "<td>" + data[i].department + "</td>";
-        render += "<td>" + data[i].lectureDay + "</td>";
-        render += "<td>" + data[i].hallNumber + "</td>";
+    var myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var data = JSON.parse(this.responseText);
+
+            for (i = 0; i < data.length; i++) {
+                render += "<tr><td>" + data[i].name + "</td><td>" + data[i].id + " </td>";
+                render += "<td>" + data[i].numberOfHours + "</td>";
+                render += "<td>" + data[i].department + "</td>";
+                render += "<td>" + data[i].lectureDay + "</td>";
+                render += "<td>" + data[i].hallNumber + "</td>";
+            }
+        }
     }
+    //var data = JSON.parse(localStorage.getItem('courses'));
+
+    myRequest.open('GET', 'http://127.0.0.1:8000/courses/',true);
+    myRequest.send();
+    
     render+="</tbody></table>";
     dynamictable.innerHTML = render; 
+}
+
+function search(){
+    var cName = document.getElementById("search1");
+    var filter = cName.nodeValue.toUpperCase();
+    var table = document.getElementById("table1");
+    var rows = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        const data = rows[i].getElementsByTagName("td");
+
+        if (filter == data[0].textContent.toUpperCase()){
+            rows[i].style.display = "";
+        }
+        else{
+            rows[i].style.display = "none";
+        }
+    }
 }
 
 WriteTable();
