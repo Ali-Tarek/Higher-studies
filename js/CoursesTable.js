3c19cd9db5ac0da6ddadcf19237400f5f806f104
Ali Tarek <alitarek115.at@gmail.com>
2023-06-01 15:58:33 +0300

    Modify the js of the courses table

--- js/CoursesTable.js
+++ js/CoursesTable.js
@@ -1,10 +1,10 @@
 function WriteTable() {
     var render = "<table class = \"courses\">";
     render += "<thead><tr><th>Course Name</th><th>Course ID</th><th>Number of hours</th><th>Department</th><th>Lecture Day</th><th>Hall Number</th></thead>";
-    render+="<tbody>";
+    render += "<tbody>";
     var myRequest = new XMLHttpRequest();
-    myRequest.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
+    myRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
 
             for (i = 0; i < data.length; i++) {
@@ -14,7 +14,7 @@ function WriteTable() {
                 render += "<td>" + data[i].day + "</td>";
                 render += "<td>" + data[i].hallnumber + "</td>";
             }
-            render+="</tbody></table>";
+            render += "</tbody></table>";
             var dynamictable = document.getElementById("dynamictable");
             dynamictable.innerHTML = render;
 
@@ -22,11 +22,11 @@ function WriteTable() {
     };
     //var data = JSON.parse(localStorage.getItem('courses'));
 
-    myRequest.open('GET', 'http://127.0.0.1:8000/courses/',true);
+    myRequest.open('GET', 'http://127.0.0.1:8000/courses/', true);
     myRequest.send();
 }
 
-function search(){
+function search() {
     var cName = document.getElementById("search1");
     var filter = cName.value.toUpperCase();
     var table = document.getElementsByClassName("courses")[0];
@@ -36,14 +36,21 @@ function search(){
         const data = rows[i].getElementsByTagName("td");
 
         if (data) {
-            if (filter == data[0].textContent.toUpperCase()){
+            if (filter === '') {
+                // Display the row using the original display value
+                rows[i].style.display = rows[i].getAttribute('data-original-display');
+            } else if (filter === data[0].textContent.toUpperCase()) {
+                // Save the original display value if it's not already saved
+                if (!rows[i].hasAttribute('data-original-display')) {
+                    rows[i].setAttribute('data-original-display', rows[i].style.display);
+                }
                 rows[i].style.display = "";
-            }
-            else{
+            } else {
                 rows[i].style.display = "none";
             }
         }
     }
 }
+
 
 WriteTable();
